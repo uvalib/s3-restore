7b5e0b8e0168cc58c26bee1186e9a885ea92a1d8
Dave Goldstein <dpg3k@virginia.edu>
2022-05-27 12:15:02 -0400

    Update example to use the uva-aws-s3 sdk

--- cmd/s3-restore/main.go
+++ cmd/s3-restore/main.go
@@ -1,27 +1,13 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"log"
 	"os"
-	"strings"
+
+	"github.com/uvalib/uva-aws-s3-sdk/uva-s3"
 )
 
-type S3Object struct {
-	BucketName  string // the bucket
-	KeyName     string // the key
-	IsGlacier   bool   // is the object stored in glacier
-	IsRestoring bool   // is the object currently being restored
-	Restored    bool   // has the object been restored
-	Size        int64  // object size
-}
-
-// taken from the examples here: https://github.com/aws/aws-sdk-go/blob/main/service/s3/examples_test.go
+var filename = "output.file"
 
 //
 // main entry point
@@ -29,167 +15,47 @@ type S3Object struct {
 func main() {
 
 	cfg := LoadConfiguration()
-	svc := s3.New(session.New())
 
-	object, err := headObject(cfg, svc)
+	s3Svc, err := uva_s3.NewUvaS3(uva_s3.UvaS3Config{Logging: true})
 	if err != nil {
-		os.Exit(1)
+		log.Fatalf("ERROR: %s", err.Error())
 	}
 
-	if object.IsGlacier == false {
+	obj := uva_s3.NewUvaS3Object(cfg.BucketName, cfg.KeyName)
+
+	o, err := s3Svc.StatObject(obj)
+	if err != nil {
+		log.Fatalf("ERROR: %s", err.Error())
+	}
+
+	if o.IsGlacier() == false {
 		log.Printf("INFO: object NOT in glacier, getting it in the normal way")
-		err = getObject(svc, object)
+		err = s3Svc.GetToFile(o, filename)
 		if err != nil {
-			os.Exit(1)
+			log.Fatalf("ERROR: %s", err.Error())
 		}
+		log.Printf("INFO: available as %s", filename)
 	} else {
-		if object.IsRestoring == true {
+		if o.IsRestoring() == true {
 			log.Printf("INFO: object in glacier, restore is IN PROGRESS...")
 		} else {
-			if object.Restored == true {
+			if o.IsRestored() == true {
 				log.Printf("INFO: object in glacier and has been restored")
-				err = getObject(svc, object)
+				err = s3Svc.GetToFile(o, filename)
 				if err != nil {
-					os.Exit(1)
+					log.Fatalf("ERROR: %s", err.Error())
 				}
+				log.Printf("INFO: available as %s", filename)
 			} else {
 				log.Printf("INFO: object in glacier, beginning a restore...")
-				err = restoreObject(cfg, svc, object)
+				err = s3Svc.RestoreObject(o, uva_s3.RESTORE_EXPEDITED, int64(cfg.RestoreDays))
 				if err != nil {
-					os.Exit(1)
+					log.Fatalf("ERROR: %s", err.Error())
 				}
 			}
 		}
 	}
 	os.Exit(0)
-}
-
-func headObject(cfg *ServiceConfig, svc *s3.S3) (S3Object, error) {
-	log.Printf("Head: s3://%s/%s", cfg.BucketName, cfg.KeyName)
-
-	object := S3Object{BucketName: cfg.BucketName, KeyName: cfg.KeyName}
-
-	input := &s3.HeadObjectInput{
-		Bucket: aws.String(cfg.BucketName),
-		Key:    aws.String(cfg.KeyName),
-	}
-
-	result, err := svc.HeadObject(input)
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case "BadRequest":
-				log.Printf("ERROR: bucket does not exist (%s)", aerr.Error())
-			case "NotFound":
-				log.Printf("ERROR: key does not exist (%s)", aerr.Error())
-			default:
-				log.Printf("ERROR: %s (%s)", aerr.Code(), aerr.Error())
-			}
-			return object, aerr
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			log.Printf("ERROR: %s", err.Error())
-			return object, err
-		}
-	} else {
-		//log.Printf("INFO: %s", result)
-	}
-
-	// get object attributes
-	object.IsGlacier = result.StorageClass != nil && strings.HasPrefix(*result.StorageClass, "GLACIER")
-	object.IsRestoring = result.Restore != nil && strings.HasPrefix(*result.Restore, "ongoing-request=\"true\"")
-	object.Restored = result.Restore != nil && strings.HasPrefix(*result.Restore, "ongoing-request=\"false\"")
-	object.Size = *result.ContentLength
-	return object, nil
-}
-
-func getObject(svc *s3.S3, object S3Object) error {
-	log.Printf("Getting: s3://%s/%s", object.BucketName, object.KeyName)
-
-	input := &s3.GetObjectInput{
-		Bucket: aws.String(object.BucketName),
-		Key:    aws.String(object.KeyName),
-		Range:  aws.String(fmt.Sprintf("bytes=0-%d", object.Size-1)),
-	}
-
-	result, err := svc.GetObject(input)
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case s3.ErrCodeNoSuchBucket:
-				log.Printf("ERROR: bucket does not exist (%s)", aerr.Error())
-			case s3.ErrCodeNoSuchKey:
-				log.Printf("ERROR: key does not exist (%s)", aerr.Error())
-			case s3.ErrCodeInvalidObjectState:
-				log.Printf("ERROR: inappropriate storage class for get (%s)", aerr.Error())
-			default:
-				log.Printf("ERROR: %s (%s)", aerr.Code(), aerr.Error())
-			}
-			return aerr
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			log.Printf("ERROR: %s", err.Error())
-			return err
-		}
-	} else {
-		//log.Printf("INFO: %s", result)
-
-		filename := "downloaded.file"
-		file, err := os.Create(filename)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(result.Body)
-		file.WriteString(buf.String())
-		log.Printf("INFO: downloaded as %s", filename)
-	}
-	return nil
-}
-
-func restoreObject(cfg *ServiceConfig, svc *s3.S3, object S3Object) error {
-	log.Printf("Restoring: s3://%s/%s", object.BucketName, object.KeyName)
-
-	input := &s3.RestoreObjectInput{
-		Bucket: aws.String(object.BucketName),
-		Key:    aws.String(object.KeyName),
-		RestoreRequest: &s3.RestoreRequest{
-			Days: aws.Int64(int64(cfg.RestoreDays)),
-			GlacierJobParameters: &s3.GlacierJobParameters{
-				Tier: aws.String("Standard"),
-			},
-		},
-	}
-
-	_, err := svc.RestoreObject(input)
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case s3.ErrCodeObjectAlreadyInActiveTierError:
-				log.Printf("ERROR: already restored (%s)", aerr.Error())
-			case s3.ErrCodeInvalidObjectState:
-				log.Printf("ERROR: inappropriate storage class for restore (%s)", aerr.Error())
-			case s3.ErrCodeNoSuchBucket:
-				log.Printf("ERROR: bucket does not exist (%s)", aerr.Error())
-			case s3.ErrCodeNoSuchKey:
-				log.Printf("ERROR: key does not exist (%s)", aerr.Error())
-			default:
-				log.Printf("ERROR: %s (%s)", aerr.Code(), aerr.Error())
-			}
-			return aerr
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			log.Printf("ERROR: %s", err.Error())
-			return err
-		}
-	} else {
-		//log.Printf("INFO: %s", result)
-	}
-	return nil
 }
 
 //
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/uvalib/s3-restore
 
 go 1.18
 
-require github.com/aws/aws-sdk-go v1.44.17
+require github.com/uvalib/uva-aws-s3-sdk/uva-s3 v0.0.0-20220527134326-5f592a4b5fe9
 
-require github.com/jmespath/go-jmespath v0.4.0 // indirect
+require (
+	github.com/aws/aws-sdk-go v1.44.23 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
